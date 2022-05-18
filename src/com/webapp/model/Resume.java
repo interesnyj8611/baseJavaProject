2be77bbbc2a0c3137efe3e242e4fc31f106822ca
interesnyj8611 <interesnyj8611@gmail.com>
2022-05-18 23:24:26 +0300

    Add equals and hashCode

--- src/com/webapp/model/Resume.java
+++ src/com/webapp/model/Resume.java
@@ -1,12 +1,26 @@
 package com.webapp.model;
 
+import java.util.Objects;
+
 /**
  * com.urise.webapp.model.com.webapp.model.Resume class
  */
 public class Resume {
 
-    // Unique identifier
     private String uuid;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Resume resume = (Resume) o;
+        return uuid.equals(resume.uuid);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(uuid);
+    }
 
     public String getUuid() {
         return uuid;
--- src/com/webapp/storage/ArrayStorage.java
+++ src/com/webapp/storage/ArrayStorage.java
@@ -2,13 +2,11 @@ package com.webapp.storage;
 
 import com.webapp.model.Resume;
 
-import java.util.Arrays;
-
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    private Resume[] storage = new Resume[10000];
+    private final Resume[] storage = new Resume[10000];
     private int size = 0;
 
     public void clear() {
